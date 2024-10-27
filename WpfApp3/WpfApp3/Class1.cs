e2ce857cc617cf8b0caf06f93f5feb4245022e4e
Obey7 <66552935+Obey7@users.noreply.github.com>
2024-10-27 15:43:11 +0000

    Add files via upload

---
+++ WpfApp3/WpfApp3/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace WpfApp3
+{
+    internal class Class1
+    { }
+        
+
+   
+        public static class TextBoxHelper
+        {
+            public static readonly DependencyProperty PlaceholderTextProperty =
+                DependencyProperty.RegisterAttached(
+                    "PlaceholderText",
+                    typeof(string),
+                    typeof(TextBoxHelper),
+                    new PropertyMetadata(string.Empty, OnPlaceholderTextChanged));
+
+            public static string GetPlaceholderText(UIElement element) =>
+                (string)element.GetValue(PlaceholderTextProperty);
+
+            public static void SetPlaceholderText(UIElement element, string value) =>
+                element.SetValue(PlaceholderTextProperty, value);
+
+            private static void OnPlaceholderTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+            {
+                if (d is TextBox textBox)
+                {
+                    textBox.GotFocus += (s, _) => RemovePlaceholder(textBox);
+                    textBox.LostFocus += (s, _) => ShowPlaceholder(textBox);
+
+                    if (string.IsNullOrEmpty(textBox.Text))
+                    {
+                        ShowPlaceholder(textBox);
+                    }
+                }
+            }
+
+            private static void ShowPlaceholder(TextBox textBox)
+            {
+                if (string.IsNullOrEmpty(textBox.Text))
+                {
+                    textBox.Text = GetPlaceholderText(textBox);
+                    textBox.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Gray);
+                }
+            }
+
+            private static void RemovePlaceholder(TextBox textBox)
+            {
+                if (textBox.Text == GetPlaceholderText(textBox))
+                {
+                    textBox.Text = string.Empty;
+                    textBox.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.White);
+                }
+            }
+        }
+    }
+
+
+
